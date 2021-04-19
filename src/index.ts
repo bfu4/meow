7e851b5922dc5ec03cda021ebbaed4638cbdd494
bfu4 <bella@sleeeepyti.me>
2021-04-19 11:34:14 -0400

    fix shit

--- src/index.ts
+++ src/index.ts
@@ -19,59 +19,63 @@ client.on("ready", () => {
 client.on("message", async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix) && message.content.substr(prefix.length).charAt(0) == ' ') {
+    return;
+  } else {
 
-  const [rawCommandName, ...args] = message.content.replace(prefix, "").split(" ");
-  const commandName = rawCommandName.toLowerCase();
+    const [ rawCommandName, ...args ] = message.content.replace(prefix, "").split(" ");
+    const commandName = rawCommandName.toLowerCase();
 
-  if (commandName === "help" && args[0]) {
-    const command = aliases.get(args[0]);
+    if (commandName === "help" && args[0]) {
+      const command = aliases.get(args[0]);
+
+      if (!command) {
+        return message.reply("⚠ Unknown Command");
+      }
+
+      const embed = new StandardEmbed(message.author)
+        .addField("Description", command.description)
+        .addField("Aliases", command.aliases.map(a => `\`${ a }\``).join(", "));
+
+      if (command.syntax) {
+        embed.addField("Syntax", `\`${ prefix }${ args[0] } ${ command.syntax }\``);
+      }
+
+      return message.reply(embed);
+    }
+
+    if (commandName === "help") {
+      const fields : EmbedField[] = commands.map(command => {
+        const name = command.aliases[0];
+
+        return {
+          name: prefix + name,
+          value: command.description,
+          inline: false,
+        };
+      });
+
+      const embed = new StandardEmbed(message.author).addFields(fields);
+
+      return message.reply(embed);
+    }
+
+    const command = aliases.get(commandName);
 
     if (!command) {
       return message.reply("⚠ Unknown Command");
     }
 
-    const embed = new StandardEmbed(message.author)
-      .addField("Description", command.description)
-      .addField("Aliases", command.aliases.map(a => `\`${a}\``).join(", "));
+    const inhibitors = Array.isArray(command.inhibitors) ? command.inhibitors : [ command.inhibitors ];
 
-    if (command.syntax) {
-      embed.addField("Syntax", `\`${prefix}${args[0]} ${command.syntax}\``);
+    try {
+      for (const inhibitor of inhibitors) {
+        await inhibitor(message, args);
+      }
+
+      await command.run(message, args);
+    } catch (e) {
+       await message.reply(`⚠ ${ e.message }`);
     }
-
-    return message.reply(embed);
-  }
-
-  if (commandName === "help") {
-    const fields: EmbedField[] = commands.map(command => {
-      const name = command.aliases[0];
-
-      return {
-        name: prefix + name,
-        value: command.description,
-        inline: false,
-      };
-    });
-
-    const embed = new StandardEmbed(message.author).addFields(fields);
-
-    return message.reply(embed);
-  }
-
-  const command = aliases.get(commandName);
-
-  if (!command) {
-    return message.reply("⚠ Unknown Command");
-  }
-
-  const inhibitors = Array.isArray(command.inhibitors) ? command.inhibitors : [command.inhibitors];
-
-  try {
-    for (const inhibitor of inhibitors) {
-      await inhibitor(message, args);
-    }
-
-    await command.run(message, args);
-  } catch (e) {
-    await message.reply(`⚠ ${e.message}`);
   }
 });
