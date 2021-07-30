7e99a8a8139ee5d65a47b349bade9abf93dd54f0
bfu4 <bella@sleeeepyti.me>
2021-07-30 16:06:55 -0400

    dumb fix

--- src/commands/fun/kitty.ts
+++ src/commands/fun/kitty.ts
@@ -14,7 +14,23 @@ export const kitty: Command = {
   async run(message: Message) {
     const titleIndex = Math.floor(Math.random() * titles.length);
     const colorIndex = Math.floor(Math.random() * colors.length);
-    const fetched = await fetch("http://aws.random.cat/meow");
+    const fetched = await fetch("http://aws.random.cat/meow", {
+      // stupid fix, endpoint broke.
+      method: "GET",
+      headers: {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0"
+      },
+    });
+    if (fetched.status !== 200) {
+      await new EmbedBuilder()
+        .setTitle("error :<")
+        .setColor(colors[0])
+        .setDescription("an api error has occurred")
+        .sendTo(message.channel)
+      return
+    }
     const res = await fetched.json();
 
     await new EmbedBuilder()
